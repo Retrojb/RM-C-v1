606cebfd14728e9fede22f7eed184821d1c81c89
Retrojb <baltescartography@gmail.com>
2020-01-11 11:59:24 -0500

    cleaned routing

--- src/app/components/houses-form/house-form.component.spec.ts
+++ src/app/components/houses-form/house-form.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HouseChoreComponent } from './house-chore.component';
+import { HouseFormComponent } from './house-form.component';
 
-describe('HouseChoreComponent', () => {
-  let component: HouseChoreComponent;
-  let fixture: ComponentFixture<HouseChoreComponent>;
+describe('HouseFormComponent', () => {
+  let component: HouseFormComponent;
+  let fixture: ComponentFixture<HouseFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HouseChoreComponent ]
+      declarations: [ HouseFormComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HouseChoreComponent);
+    fixture = TestBed.createComponent(HouseFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
